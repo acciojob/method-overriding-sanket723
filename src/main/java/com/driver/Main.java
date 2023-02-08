ed0bba9d3194287fd755596211d3ca5f19254ec9
sanket723 <sanketpawar723@gmail.com>
2023-02-08 13:28:45 +0530

    first submit override assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,32 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      //Task-3
+      B b1 = new B();
+      b1.meth();
+
+      //Task-5
+      B b2 = new B();
+      b2.meth();
+
+  }
 }
+
+//Task-1
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+//Task-2
+class B extends A{
+    //Task-4
+    String meth(){
+        return "Method is overridden in Extended class B";
+    }
+}
+
+
+
